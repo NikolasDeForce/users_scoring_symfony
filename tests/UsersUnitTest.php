c06cccbcb4ac40491bcd48ddeac3c7f8c3208f7c
Николай Притыкин <nikolajpritykin@MacBook-Air-Nikolaj.local>
2025-04-28 09:42:26 +0700

    add unit test --done

--- tests/UsersUnitTest.php
+++ tests/UsersUnitTest.php
@@ -4,19 +4,27 @@ namespace App\Tests;
 
 use App\Entity\Users;
 use PHPUnit\Framework\TestCase;
-use Symfony\Component\Form\FormInterface;
+use Src\ScoringTest\ScoringTest;
 
 class UsersUnitTest extends TestCase
 {
     public function testScoringFunc(): void
     {
-        $user = Users::class;
-        $form = FormInterface::class;
+        $user = new Users();
+        $user->setFirstName('UserFirst');
+        $user->setLastName('LastName');
+        //БИЛАЙН +5
+        $user->setPhone('+7964093112');
+        //GMAIL +10
+        $user->setEmail('user@gmail.com');
+        //Высшее образование +15
+        $user->setEducation('Высшее образование');
+        //Согласие на обработку = true +4
+        $user->setIsAcceptData('1');
 
-        $users_data = $form->createForm(Users::class, [
+        $score = ScoringTest::scoring_test($user);
 
-        ]);
-
-
+        //Общее кол-во баллов 34, сравним с функцией
+        $this->assertEquals(34, $score);
     }
 }
