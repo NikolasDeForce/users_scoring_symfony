ac41814615f56ad1f924739d9a8927190c5b02b1
Николай Притыкин <nikolajpritykin@MacBook-Air-Nikolaj.local>
2025-04-28 08:54:52 +0700

    refactoring

--- tests/UsersUnitTest.php
+++ tests/UsersUnitTest.php
@@ -2,12 +2,21 @@
 
 namespace App\Tests;
 
+use App\Entity\Users;
 use PHPUnit\Framework\TestCase;
+use Symfony\Component\Form\FormInterface;
 
 class UsersUnitTest extends TestCase
 {
-    public function testSomething(): void
+    public function testScoringFunc(): void
     {
-        $this->assertTrue(true);
+        $user = Users::class;
+        $form = FormInterface::class;
+
+        $users_data = $form->createForm(Users::class, [
+
+        ]);
+
+
     }
 }
